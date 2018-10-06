e9f94d9bb0322fbeaabc30b1df5e3686826937a9
Tae Hyoungman <better110@naver.com>
2018-10-06 18:12:00 +0900

    Changed benchmark.py and hhh.py to benchmark per pool

--- apache-bench/benchmark.py
+++ apache-bench/benchmark.py
@@ -2,21 +2,27 @@ import subprocess
 import sys
 
 
+POOLS = [4, 16]
+
+
 if __name__ == '__main__':
-    count = 0
     pool = 4
-    tsv_files = []
+    tsv_files = [[] for _ in POOLS]
 
     with open('servers.txt') as servers_file:
-        for server in servers_file.readlines():
-            filename = 'test-{}.tsv'.format(count + 1)
-            tsv_files.append(filename)
-            cmdline = './run-ab.sh {} {} {}'.format(server.rstrip(), pool ** (count + 1), filename)
-            print(cmdline)
-            subprocess.call(['bash', '-c', cmdline], stdout=sys.stdout, stderr=sys.stderr)
-            count += 1
+        for i, server in enumerate(servers_file.readlines()):
 
-    files_cmd = ' '.join(tsv_files)
-    cmdline = 'python3 ../visuals/hhh.py {}'.format(files_cmd)
-    print(cmdline)
-    subprocess.call(['bash', '-c', cmdline], stdout=sys.stdout, stderr=sys.stderr)
+            for pi, pool in enumerate(POOLS):
+                filename = 'test-{:0>2}-{}.tsv'.format(pool, chr(ord('a') + i))
+                tsv_files[pi].append(filename)
+                cmdline = './run-ab.sh {} {} {}'.format(server.rstrip(), pool, filename)
+                print(cmdline)
+                subprocess.call(['bash', '-c', cmdline], stdout=sys.stdout, stderr=sys.stderr)
+
+    for pi, pool in enumerate(POOLS):
+        pool_tsvs = tsv_files[pi]
+        files_cmd = ' '.join(pool_tsvs)
+        cmdline = ('python3 ../visuals/hhh.py test-{0:0>2}.svg "Test with {0:0>2} pools" {1}'
+                   ''.format(pool, files_cmd))
+        print(cmdline)
+        subprocess.call(['bash', '-c', cmdline], stdout=sys.stdout, stderr=sys.stderr)
--- visuals/hhh.py
+++ visuals/hhh.py
@@ -3,27 +3,37 @@ import pygal
 import sys
 
 
+HOP_LIMIT = 4999
+HOP_STEP = 20
+
 def read_tsv(filename: str) -> list:
-    count_per_hop = {}
+    counts_per_hop = [0 for _ in range((HOP_LIMIT + 1) // HOP_STEP)]
     with open(filename) as tsv:
         for row in csv.DictReader(tsv, delimiter="\t"):
-            ttime = row['ttime'][:-1] + '0'
-            orig_count = count_per_hop.get(ttime, 0)
-            count_per_hop[ttime] = orig_count + 1
-    hop_counts = []
-    for key, val in count_per_hop.items():
-        hop_counts.append((int(key), val))
-    return hop_counts
+            ttime_index = int(row['ttime']) // HOP_STEP
+            counts_per_hop[ttime_index] += 1
+    return counts_per_hop
 
 
 if __name__ == '__main__':
-    line_chart =  pygal.Line()
-    line_chart = pygal.XY()
-    line_chart.x_title = 'Response Time'
-    line_chart.y_title = 'Request Count'
+    print('The first argument is the output file name, the second argument is its title, '
+          'and followings are TSV files.')
 
-    for i, tsv_file in enumerate(sys.argv[1:]):
+    custom_style = pygal.style.Style()
+
+    line_chart = pygal.Line(
+        x_label_rotation=1,
+        x_labels_major_every=HOP_LIMIT // (HOP_STEP * 10),
+        show_minor_x_labels=False,
+        style=custom_style
+    )
+    line_chart.title = sys.argv[2]
+    line_chart.x_title = 'Connection Time'
+    line_chart.y_title = 'Request Count'
+    line_chart.x_labels = [ttime for ttime in range(0, HOP_LIMIT, HOP_STEP)]
+
+    for i, tsv_file in enumerate(sys.argv[3:]):
         hop_counts = read_tsv(tsv_file)
-        line_chart.add('Test ' + str(i + 1), hop_counts,stroke_style={'width':4},dots_size=0)
+        line_chart.add('Cluster ' + chr(ord('A') + i), hop_counts, stroke_style={'width':4},dots_size=0)
     
-    line_chart.render_to_file('test2.svg')
+    line_chart.render_to_file(sys.argv[1])
