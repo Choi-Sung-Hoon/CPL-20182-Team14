3cb93df406d7852b6a247b9d9f96d67f5b6ac70d
Tae Hyoungman <better110@naver.com>
2018-10-06 20:44:38 +0900

    Added "visuals only option" to benchmark.py

--- apache-bench/benchmark.py
+++ apache-bench/benchmark.py
@@ -7,22 +7,28 @@ POOLS = [4, 16]
 
 if __name__ == '__main__':
     pool = 4
-    tsv_files = [[] for _ in POOLS]
+    servers_count = 0
 
-    with open('servers.txt') as servers_file:
-        for i, server in enumerate(servers_file.readlines()):
+    if len(sys.argv) == 1:
+        with open('servers.txt') as servers_file:
+            for i, server in enumerate(servers_file.readlines()):
 
-            for pi, pool in enumerate(POOLS):
-                filename = 'test-{:0>2}-{}.tsv'.format(pool, chr(ord('a') + i))
-                tsv_files[pi].append(filename)
-                cmdline = './run-ab.sh {} {} {}'.format(server.rstrip(), pool, filename)
-                print(cmdline)
-                subprocess.call(['bash', '-c', cmdline], stdout=sys.stdout, stderr=sys.stderr)
+                for pi, pool in enumerate(POOLS):
+                    filename = 'test-{:0>2}-{}.tsv'.format(pool, chr(ord('a') + i))
+                    cmdline = './run-ab.sh {} {} {}'.format(
+                        server.rstrip(), pool, filename)
+                    print(cmdline)
+                    subprocess.call(['bash', '-c', cmdline],
+                                    stdout=sys.stdout, stderr=sys.stderr)
+                    servers_count += 1
+    else:
+        servers_count = int(sys.argv[1])
 
     for pi, pool in enumerate(POOLS):
-        pool_tsvs = tsv_files[pi]
-        files_cmd = ' '.join(pool_tsvs)
-        cmdline = ('python3 ../visuals/hhh.py test-{0:0>2}.svg "Test with {0:0>2} pools" {1}'
+        files_cmd = ' '.join(['test-{:0>2}-{}.tsv'
+                              ''.format(pool, chr(ord('a') + i)) for i in range(servers_count)])
+        cmdline = ('python3 ../visuals/hhh.py test-{0:0>2}.svg "Test with {0} pools" {1}'
                    ''.format(pool, files_cmd))
         print(cmdline)
-        subprocess.call(['bash', '-c', cmdline], stdout=sys.stdout, stderr=sys.stderr)
+        subprocess.call(['bash', '-c', cmdline],
+                        stdout=sys.stdout, stderr=sys.stderr)
