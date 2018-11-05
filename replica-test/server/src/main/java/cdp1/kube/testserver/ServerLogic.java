4da2f40df302244ec7ddb4bd5065597fc769a8fb
warhand <wise_9@naver.com>
2018-11-05 19:53:44 +0900

    ServerLogic

--- replica-test/server/src/main/java/cdp1/kube/testserver/ServerLogic.java
+++ replica-test/server/src/main/java/cdp1/kube/testserver/ServerLogic.java
@@ -1,5 +1,64 @@
 package cdp1.kube.testserver;
 
-class ServerLogic {
+import java.util.Scanner;
 
+class ServerLogic {
+	int nth;
+	
+	public ServerLogic(int n)
+	{
+		this.nth = n;
+	}
+
+	int getNthPrimeNumber()
+	{
+		int cnt=0;
+		int number=2;
+		
+		while(true)
+		{
+			if(isPrimeNumber(number))
+			{
+				cnt++;
+				if(cnt==nth)
+					break;
+			}
+			
+			number++;
+		}
+		
+		return number;
+	}
+	
+	boolean isPrimeNumber(int n)
+	{
+		boolean isPrime = true;
+		int i = 2;
+		
+		while(i!=n)
+		{
+			if(n%i==0)
+			{
+				isPrime = false;
+			}
+			
+			i++;
+		}
+		
+		return isPrime;
+	}
+	
+	/*public static void main(String[] args)
+	{
+		ServerLogic test;
+		int n;
+		int primeN;
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("get Nth prime number : ");
+		n = scan.nextInt();
+		test = new ServerLogic(n);
+		primeN = test.getNthPrimeNumber();
+		System.out.println("nth Prime number : " + primeN);
+	}*/
 }
