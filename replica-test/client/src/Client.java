b4194133537c3736d971878589c1f6b151f437ae
CSH <33472400+Choi-Sung-Hoon@users.noreply.github.com>
2018-11-20 20:03:20 +0900

    Client with an infinite loop

--- replica-test/client/src/Client.java
+++ replica-test/client/src/Client.java
@@ -31,38 +31,40 @@ public class Client
 		int port = Integer.parseInt(args[0]);
 		long startTime, stopTime;
 		
-		try(Socket socket = new Socket(hostname, port))
-		{
-			System.out.println("Client is connected");
-			
-			InputStream input = socket.getInputStream();
-			OutputStream output = socket.getOutputStream();
-			BufferedReader reader = new BufferedReader(new InputStreamReader(input));
-			PrintWriter writer = new PrintWriter(output, true);
-			String response;
-			
-			// create a graph
-			Client swingWorkerRealTime = new Client();
-			swingWorkerRealTime.go();
-			
-			// input
-			writer.println(Integer.toString(3000));
-			
-			startTime = System.nanoTime();
-			while((response=reader.readLine())!=null)
-				System.out.println(response);
-			stopTime = System.nanoTime();
-
-			elapsedTime = (stopTime - startTime) / 1000000000.0;
-			System.out.println("Time : " + elapsedTime);
-		}
-		catch(Exception e)
-		{
-			e.printStackTrace();
+		// create a graph
+		Client swingWorkerRealTime = new Client();
+		swingWorkerRealTime.go();
+		
+		for(int i = 1; i <= 5000; i += 10){
+			try(Socket socket = new Socket(hostname, port))
+			{
+				System.out.println("Client is connected");
+				
+				InputStream input = socket.getInputStream();
+				OutputStream output = socket.getOutputStream();
+				BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+				PrintWriter writer = new PrintWriter(output, true);
+				String response;
+				
+				// input
+				writer.println(Integer.toString(i));
+				
+				startTime = System.currentTimeMillis();
+				while((response=reader.readLine())!=null)
+					System.out.println(response);
+				stopTime = System.currentTimeMillis();
+	
+				elapsedTime = stopTime - startTime;
+				System.out.println("Time : " + elapsedTime);
+			}
+			catch(Exception e)
+			{
+				e.printStackTrace();
+			}
 		}
 		
-		System.out.println("Client End.");
-		scanner.close();
+		//System.out.println("Client End.");
+		//scanner.close();
 	}
 
 	private void go()
@@ -74,7 +76,7 @@ public class Client
 		chart.getStyler().setXAxisTicksVisible(false);
 		chart.getStyler().setXAxisMax(100.0);
 		chart.getStyler().setXAxisMin(0.0);
-		chart.getStyler().setYAxisMax(100.0);
+		chart.getStyler().setYAxisMax(1000.0);
 		chart.getStyler().setYAxisMin(0.0);
 
 		// Show it
@@ -136,7 +138,7 @@ public class Client
 			long duration = System.currentTimeMillis() - start;
 			try
 			{
-				Thread.sleep(20 - duration); // 40 ms ==> 25fps
+				Thread.sleep(40 - duration); // 40 ms ==> 25fps
 			}
 			catch (InterruptedException e)
 			{
