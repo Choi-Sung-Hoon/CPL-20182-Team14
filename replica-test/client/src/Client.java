fd9e9de078606f11ebd9475dd3cffb47f34ed8ac
warhand <wise_9@naver.com>
2018-11-24 16:31:47 +0900

    client 수정

--- replica-test/client/src/Client.java
+++ replica-test/client/src/Client.java
@@ -1,13 +1,4 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.PrintWriter;
-import java.net.Socket;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.ExecutorService;
